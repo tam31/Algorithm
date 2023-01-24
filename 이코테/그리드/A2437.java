42e85c3be7b48756496583def39f192802ce252d
tam31 <98944772+tam31@users.noreply.github.com>
2023-01-24 21:30:16 +0900

    Add files via upload

---
+++ 이코테/그리드/A2437.java
@@ -0,0 +1,34 @@
+package 그리드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class A2437 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int target=1;
+		for(int k: arr) {
+			if(target <k) {
+				break;
+			}
+			target +=k;
+		}
+		System.out.println(target);
+
+	}
+
+}
