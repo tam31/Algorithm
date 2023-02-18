5bed65c3deafaed463cca6d360e87498e801c78d
tam31 <98944772+tam31@users.noreply.github.com>
2023-02-18 14:54:22 +0900

    Add files via upload

---
+++ 바킹독/백트래킹/A07_15654.java
@@ -0,0 +1,57 @@
+package 백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class A07_15654 {
+	static int n;
+	static int m;
+	static int[] arr;
+	static int[] ans;
+	static boolean[] vis;
+	static StringBuilder sb = new StringBuilder(); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n];
+		ans = new int[m];
+		vis = new boolean[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+//		System.out.println(Arrays.toString(arr));
+		
+		func(0);
+		System.out.println(sb);
+		
+	}
+	private static void func(int k) {
+		if(k == m) {
+			for(int i=0; i<m; i++) {
+				sb.append(ans[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=0; i<n; i++) {
+			if(!vis[i]) {
+				vis[i] = true;
+				ans[k] = arr[i];
+				func(k+1);
+				vis[i] = false;
+			}
+		}
+	}
+
+}
