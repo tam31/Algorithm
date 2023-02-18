69e928cff9bd6edab8e554822b1fd2b058d8cee0
tam31 <98944772+tam31@users.noreply.github.com>
2023-02-18 17:41:10 +0900

    Add files via upload

---
+++ 바킹독/백트래킹/A10_15657.java
@@ -0,0 +1,50 @@
+package 백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class A10_15657 {
+	static int n;
+	static int m;
+	static int[] arr;
+	static int[] ans;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n];
+		ans = new int[m];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		func(0,0);
+		System.out.println(sb);
+		
+	}
+	private static void func(int k, int z) {
+		if(k==m) {
+			for(int i=0; i<m; i++) {
+				sb.append(ans[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=z; i<n; i++) {
+			ans[k] = arr[i];
+			func(k+1,i);
+		}
+	}
+
+}
