92a2359ea8a1fb26477437735e65690702c18e5f
tam31 <98944772+tam31@users.noreply.github.com>
2023-02-22 19:56:20 +0900

    Add files via upload

--- 오답/오답2/A13913.java
+++ 오답/오답2/A13913.java
@@ -1,4 +1,4 @@
-package 오답;
+package 오답2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,24 +13,24 @@ public class A13913 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st= new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken());
+		int n= Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
 		int[] arr = new int[100001];
 		int[] vis = new int[100001];
 		
 		Arrays.fill(arr, -1);
-		arr[n] = 0;
-		vis[n] = n;
 		
 		Queue<Integer> que = new LinkedList<>();
 		que.add(n);
+		arr[n] = 0;
+		vis[n] = n;
 		
 		while(!que.isEmpty()) {
 			int data = que.poll();
-			
+
 			if(data == m) {
 				System.out.println(arr[m]);
 				break;
@@ -38,31 +38,33 @@ public class A13913 {
 			
 			if(data-1>=0 && arr[data-1] == -1) {
 				arr[data-1] = arr[data]+1;
-				que.add(data-1);
 				vis[data-1] = data;
+				que.add(data-1);
 			}
-			if(data+1<=100000 && arr[data+1] == -1) {
+			
+			if(data+1<100001 && arr[data+1] == -1) {
 				arr[data+1] = arr[data]+1;
-				que.add(data+1);
 				vis[data+1] = data;
+				que.add(data+1);
 			}
-			if(data*2<=100000 && arr[data*2] == -1) {
+			
+			if(data*2<100001 && arr[data*2] == -1) {
 				arr[data*2] = arr[data]+1;
-				que.add(data*2);
 				vis[data*2] = data;
+				que.add(data*2);
 			}
 		}
 		
 		Stack<Integer> stack = new Stack<>();
 		stack.add(m);
-		while(stack.peek()!=n) {
+		
+		while(stack.peek() != n) {
 			stack.add(vis[stack.peek()]);
 		}
 		
 		while(!stack.isEmpty()) {
 			System.out.print(stack.pop()+" ");
 		}
-		
 	}
 
 }
