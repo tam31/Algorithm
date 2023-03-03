875b21030ea04833d75beb2a43530a102fd3165c
tam31 <98944772+tam31@users.noreply.github.com>
2023-03-03 22:32:40 +0900

    Add files via upload

--- 오답/A1699.java
+++ 오답/A1699.java
@@ -1,4 +1,4 @@
-package 오답2;
+package 오답;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,23 +6,18 @@ import java.io.InputStreamReader;
 
 public class A1699 {
 
-	public static void main(String[] args) throws IOException {
-		//입력
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
 		int[] dp = new int[n+1];
-		dp[1]=1;
-		for(int i=2; i<=n; i++) {
+		for(int i=1; i<=n; i++) {
 			dp[i] = i;
 			for(int j=1; j*j<=i; j++) {
-				dp[i] = Math.min(dp[i-j*j]+1, dp[i]);
+				dp[i] = Math.min(dp[i], dp[i-j*j]+1);
 			}
 		}
-		
 		System.out.println(dp[n]);
-		
-
 	}
 
 }
