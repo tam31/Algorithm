991c7207de39e279afe7a21faa23180fd648c2c2
tam31 <98944772+tam31@users.noreply.github.com>
2023-03-06 15:40:45 +0900

    Add files via upload

--- 오답/A10942.java
+++ 오답/A10942.java
@@ -3,29 +3,45 @@ package 오답;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class A11052 {
+public class A10942 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
 		int[] arr = new int[n+1];
+		int[][] dp = new int[n+1][n+1];
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int[] dp = new int[n+1];
-		for(int i=1; i<=n; i++) {
-			dp[i] = arr[i];
-			for(int j=1; j<i; j++) {
-				dp[i] = Math.max(dp[i], dp[j]+dp[i-j]);
+			dp[i][i]=1;
+			
+			if(i!=1 && arr[i] == arr[i-1]) {
+				dp[i-1][i] = 1;
 			}
 		}
-		System.out.println(dp[n]);
+		
+		for(int i=2; i<=n; i++) {
+			for(int j=1; i+j<=n; j++) {
+				if(arr[j] == arr[i+j] && dp[j+1][i+j-1]==1) {
+					dp[j][i+j] = 1;
+				}
+			}
+		}
+		
+		int m = Integer.parseInt(br.readLine());
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.println(dp[a][b]);
+		}
+		
+		
+		
 	}
 
 }
