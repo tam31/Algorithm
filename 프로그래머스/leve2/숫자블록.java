84784b406f4037c7c9488b7896765bcd294ee4da
tam31 <98944772+tam31@users.noreply.github.com>
2023-03-21 15:31:34 +0900

    Add files via upload

---
+++ 프로그래머스/leve2/숫자블록.java
@@ -0,0 +1,19 @@
+package leve2;
+
+public class 숫자블록 {
+
+	public static void main(String[] args) {
+		int n = 10;
+		String[] arr = {"4","1","2"};
+		
+		String ans = "";
+		while(n!=0) {
+			int num = n%3;
+			n = n/3;
+			if(num == 0) n -=1;
+			ans = arr[num]+ans;
+		}
+		System.out.println(ans);
+	}
+
+}
