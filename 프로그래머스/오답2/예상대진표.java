fb2792fb53322e9297026651a97dc8bcacb4b389
tam31 <98944772+tam31@users.noreply.github.com>
2023-04-08 21:11:46 +0900

    Add files via upload

---
+++ 프로그래머스/오답2/예상대진표.java
@@ -0,0 +1,19 @@
+package 오답2;
+
+public class 예상대진표 {
+
+	public static void main(String[] args) {
+		int N = 8;
+		int A = 4;
+		int B = 7;
+		
+		int cnt = 0;
+		while(A!=B) {
+			A = (A+1)/2;
+			B = (B+1)/2;
+			cnt +=1;
+		}
+		System.out.println(cnt);
+	}
+
+}
