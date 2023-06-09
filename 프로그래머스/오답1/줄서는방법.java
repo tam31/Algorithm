3cc64276e001ba9dd2722cbb882c787364c3b609
tam31 <98944772+tam31@users.noreply.github.com>
2023-06-09 23:12:43 +0900

    Add files via upload

--- 프로그래머스/오답1/줄서는방법.java
+++ 프로그래머스/오답1/줄서는방법.java
@@ -1,33 +1,30 @@
-package 오답1;
-import java.util.*;
+package leve2;
+
+import java.util.ArrayList;
+
 public class 줄서는방법 {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int n = 3;
-		int k= 5;
+		int k =5;
 		
-		int ans[] = new int[n];
-		List<Integer> list = new ArrayList<>();
-		int f = 1;
+		int[] ans = new int[n];
+		ArrayList<Integer> list = new ArrayList<>();
+		long f = 1;
 		for(int i=1; i<=n; i++) {
-			f*=i;
+			f *=i;
 			list.add(i);
 		}
 		
 		k -=1;
 		int idx = 0;
-		while(n>0) {
-			f = f/n;
-			int num = k/f;
-			ans[idx] = list.get(num);
-			list.remove(num);
-			
-			k = k%f;
-			idx +=1;
-			n -= 1;
+		while(idx<n) {
+			f = f/(n-idx);
+			ans[idx++] = list.remove((int)(k/f));
+			k %= f;
 		}
 		
-		System.out.println(Arrays.toString(ans));
 	}
 
 }
