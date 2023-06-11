6e4f3267df0975c61f716102c3edefe0ac769f44
tam31 <98944772+tam31@users.noreply.github.com>
2023-06-11 23:29:43 +0900

    Add files via upload

--- 백준/class3.1/A1463.java
+++ 백준/class3.1/A1463.java
@@ -1,9 +1,8 @@
-package class3;
-
-import java.util.*;
+package ¿¬½À;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class A1463 {
 
@@ -11,28 +10,18 @@ public class A1463 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
-		int cnt = 0;
-		Queue<int[]> que = new LinkedList<>();
-		que.add(new int[] {n,0});
-		while(!que.isEmpty()) {
-			int[] a = que.poll();
-			
-			if(a[0]==1) {
-				cnt = a[1];
-				break;
+		int[] dp = new int[n+1];
+		for(int i=2; i<=n; i++) {
+			dp[i] = i-1;
+			if(i%3 ==0) {
+				dp[i] = Math.min(dp[i], dp[i/3]+1);
 			}
-			
-			if(a[0]%3==0) {
-				que.add(new int[] {a[0]/3,a[1]+1});
+			if(i%2 ==0) {
+				dp[i] = Math.min(dp[i], dp[i/2]+1);
 			}
-			if(a[0]%2==0) {
-				que.add(new int[] {a[0]/2,a[1]+1});
-			}
-			if(a[0]-1>0) {
-				que.add(new int[] {a[0]-1,a[1]+1});
-			}
+			dp[i] = Math.min(dp[i], dp[i-1]+1);
 		}
-		System.out.println(cnt);
+		System.out.println(dp[n]);
 	}
 
 }
