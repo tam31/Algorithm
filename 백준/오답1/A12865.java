e38eaf3351071f08531836acef4e94939a20b2f4
tam31 <98944772+tam31@users.noreply.github.com>
2023-06-24 02:34:05 +0900

    Add files via upload

--- 백준/오답1/A12865.java
+++ 백준/오답1/A12865.java
@@ -1,4 +1,4 @@
-package 오답;
+package 연습;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,20 +10,22 @@ public class A12865 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int n = Integer.parseInt(st.nextToken());
-		int m =Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		
 		int[][] arr = new int[n+1][2];
 		for(int i=1; i<=n; i++) {
 			st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			arr[i][0] = a;
+			arr[i][1] = b;
 		}
 		
 		int[][] dp = new int[n+1][m+1];
 		for(int i=1; i<=n; i++) {
-			
 			for(int j=1; j<=m; j++) {
-				if(arr[i][0] <=j) {
+				if(j>=arr[i][0]) {
 					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i][0]]+arr[i][1]);
 				}else {
 					dp[i][j] = dp[i-1][j];
@@ -31,7 +33,7 @@ public class A12865 {
 			}
 		}
 		
-		System.out.println(dp[n][m]);
+		System.out.println(dp[n-1][m]);
 	}
 
 }
