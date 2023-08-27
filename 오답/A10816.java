5b939d9e0327cc4a11d163b5379ec100d99661d9
Tea Min <98944772+tam31@users.noreply.github.com>
2023-08-27 23:48:52 +0900

    이분탐색-기본-오답1

--- 오답/A10816.java
+++ 오답/A10816.java
@@ -1,4 +1,4 @@
-package 이분탐색;
+package 이분탐색.오답;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,11 +8,12 @@ public class A10816 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] list = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++) {
 			list[i] = Integer.parseInt(st.nextToken());
 		}
+		
 		Arrays.sort(list);
 		
 		StringBuilder sb = new StringBuilder();
@@ -20,37 +21,39 @@ public class A10816 {
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<m; i++) {
 			int tmp = Integer.parseInt(st.nextToken());
-			int re = upper(list,tmp)-lower(list,tmp);
-			sb.append(re+" ");
+			int a =func1(tmp,list);
+			int b = func2(tmp,list);
+			sb.append((b-a)+" ");
 		}
 		System.out.println(sb);
+		
+		
 	}
-	private static int lower(int[] list,int tmp) {
+	private static int func1(int tmp, int[] list) {
 		int start = 0;
 		int end = list.length;
 		while(start<end) {
-			
 			int mid = (start+end)/2;
-			if(list[mid] >=tmp) {
-				end = mid;
-			}else {
+			if(list[mid]<tmp) {
 				start = mid+1;
+			}else {
+				end = mid;
+			}
+		}
+		return start;
+	}
+	private static int func2(int tmp, int[] list) {
+		int start = 0;
+		int end = list.length;
+		while(start<end) {
+			int mid = (start+end)/2;
+			if(list[mid]<=tmp) {
+				start = mid+1;
+			}else {
+				end = mid;
 			}
 		}
 		return start;
 	}
 	
-	private static int upper(int[] list, int tmp) {
-		int start = 0;
-		int end = list.length;
-		while(start<end) {
-			int mid = (start+end)/2;
-			if(list[mid]> tmp) {
-				end = mid;
-			}else {
-				start = mid+1;
-			}
-		}
-		return start;
-	}
 }
